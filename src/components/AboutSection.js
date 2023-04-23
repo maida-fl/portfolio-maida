8771c2913f65a249312a2d26d41aeb2832e2ae85
maida-fl <maida.guzman@outlook.com>
2023-04-23 11:35:55 -0300

    about section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>Maida Guzman</h2>
+          </div>
+          <div className="hide">
+            <p>
+              web <span>developer</span>
+            </p>
+          </div>
+          <button>Contact</button>
+        </div>
+        <div className="image">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
