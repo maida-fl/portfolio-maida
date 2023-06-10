3f1074fb53258aaf9f9804355706f843d7f22a2e
maida-fl <maida.guzman@outlook.com>
2023-06-10 12:03:57 -0300

    content and blurring on hover

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,10 +5,14 @@ import { motion } from "framer-motion";
 const Project = (props) => {
   return (
     <StyledProject>
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
-      <img src={props.image} alt="styled" />
-      <div className="blur"></div>
+      <div className="image">
+        <img src={props.image} alt="styled" />
+        <div className="blur"></div>
+        <div className="content">
+          <h3>{props.name}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
     </StyledProject>
   );
 };
@@ -30,6 +34,38 @@ const StyledProject = styled(motion.div)`
     object-fit: cover;
     object-position: top;
   }
+
+  .content {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+    transition: all 0.8s ease;
+    z-index: 1;
+    &:hover {
+      opacity: 1;
+    }
+    h3,
+    p {
+      color: black;
+    }
+  }
+
+  .image {
+    position: relative;
+    &:hover .blur {
+      background: rgba(255, 255, 255, 0.8);
+      backdrop-filter: blur(8px);
+      -webkit-backdrop-filter: blur(8px);
+    }
+  }
+
   .blur {
     width: 100%;
     height: 100%;
@@ -37,11 +73,6 @@ const StyledProject = styled(motion.div)`
     right: 0;
     position: absolute;
     transition: all 0.8s ease;
-    &:hover {
-      background: rgba(255, 255, 255, 0.3);
-      backdrop-filter: blur(8px);
-      -webkit-backdrop-filter: blur(8px);
-    }
   }
 `;
 
